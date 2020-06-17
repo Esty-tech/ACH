9e8839d4914f1f8b0b2b39b0b4db4682fd2f169b
Esty-tech <63051730+Esty-tech@users.noreply.github.com>
2020-06-17 14:24:24 -0300

    cambio

---
+++ app.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    return name + "QUE GUSTO TENERTE POR AQUI";
+}
+greet("Amigo");
+
+
+
+<script>
+var d = new Date();
+document.getElementById("demo").innerHTML = d;
+</script>
