9499b9d641e226ef1a303be5df5925b28b153feb
Esty-tech <63051730+Esty-tech@users.noreply.github.com>
2020-06-18 09:52:59 -0300

    noo

---
+++ index.js
@@ -0,0 +1,24 @@
+function greet(name) {
+    return name + "QUE GUSTO TENERTE POR AQUI";
+}
+greet("Amigo");
+console.log(greet);
+
+
+
+
+
+let name = 'Esterline';
+console.log(name);
+
+
+
+let interestRate = 0.3;
+interesRate = 0.3;
+console.log(interestRate);
+
+let person = {
+    name: 'Esterline'
+    age:30
+};
+console.log(person);
